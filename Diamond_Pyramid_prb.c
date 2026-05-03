cebf0403989e422a6f2a7fc8468f2336899fbf2f
zamirul <javshazib@gmail.com>
2026-05-03 06:10:17 +0600

    Inverted Left Triangle

---
+++ Diamond_Pyramid_prb.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    // Upper Pyramid
+    int star = 1;
+    int space = n - 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 1; k <= star; k++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+        star += 2;
+        space--;
+    }
+
+    // Lower Pyramid
+    star = star - 4;
+    space = 1;
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 1; k <= star; k++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+        star -= 2;
+        space++;
+    }
+
+    return 0;
+}
