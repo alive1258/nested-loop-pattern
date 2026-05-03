a32f6add8bc7f6644e10260cdb840230ba80a457
zamirul <javshazib@gmail.com>
2026-05-03 06:24:21 +0600

    Same Number Triangle

---
+++ Hollow_Pyramid.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        // spaces
+        for (int j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+
+        // stars & hollow logic
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            if (j == 1 || j == 2 * i - 1 || i == n)
+                printf("*");
+            else
+                printf(" ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
